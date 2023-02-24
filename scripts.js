fc6491c63d627f8ccbef6c0359f25324f0e1f032
easterlord <120390956+easterlord@users.noreply.github.com>
2023-02-24 19:46:40 +0545

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// This is an example of a JavaScript function that would be included in a separate script.js file and linked to the HTML file.
+
+function toggleMenu() {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('active');
+}
